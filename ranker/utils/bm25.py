6ece391b64b1ae1342e23bac6214d200983f4027
ImmortalCi <74886609+ImmortalCi@users.noreply.github.com>
2022-02-23 15:36:40 +0800

    2.23 cmedQA数据集下BM25算法实现

--- ranker/utils/bm25.py
+++ ranker/utils/bm25.py
@@ -10,14 +10,14 @@ class BM25(object):
 
     def __init__(self, docs):
         self._D = len(docs)
-        self._dl = torch.tensor([len(doc) for doc in docs])
+        self._dl = torch.tensor([len(doc) for doc in tqdm(docs)])
         self._avg_dl = self._dl.sum().float() / self._D
-        self._words = set([word.lower() for doc in docs for word in doc])
-        self._word2id = {word: i for i, word in enumerate(self._words)}
+        self._words = set([word.lower() for doc in tqdm(docs) for word in doc])
+        self._word2id = {word: i for i, word in tqdm(enumerate(self._words))}
         self._n_words = len(self._words)
 
         f = []
-        for doc in docs:
+        for doc in tqdm(docs):
             word_ids = torch.tensor([self._word2id[word.lower()] for word in doc])
             count = (word_ids.unsqueeze(1) == torch.arange(self._n_words).unsqueeze(0)).sum(0)
             f.append(count)
@@ -58,14 +58,15 @@ class BM25(object):
 
 
 if __name__ == '__main__':
+    torch.set_num_threads(8)
     parser = argparse.ArgumentParser(
         description='BM25'
     )
-    parser.add_argument('--train_file', default='data/answer.csv',
+    parser.add_argument('--train_file', default='../../data/answer.csv',
                         help='train file')
-    parser.add_argument('--test_file', default='data/test_candidates.txt',
+    parser.add_argument('--test_file', default='../../data/test_candidates.txt',
                         help='test file')
-    parser.add_argument('--question', default='data/question.csv',
+    parser.add_argument('--question', default='../../data/question.csv',
                         help='all questions')
     parser.add_argument('--candidates_num', default='30',
                         help='the number of candidates')
@@ -77,6 +78,7 @@ if __name__ == '__main__':
     answer_list = []
     for i in range(answer_data.shape[0]):
         answer_list.append(answer_data.loc[i, 'content'])
+    print('--stage1')
 
     # 读取测试集
     query2answer_id = dict()
@@ -89,23 +91,31 @@ if __name__ == '__main__':
                     query2answer_id[test_pair[0]] = [test_pair[1]]
                 else:
                     query2answer_id[test_pair[0]].append(test_pair[1])
+    print('--stage2')
 
     # 读取问题
     id2question = dict()
     question_dataframe = pd.read_csv(args.question)
     for i in range(question_dataframe.shape[0]):
         id2question[question_dataframe.loc[i, 'question_id']] = question_dataframe.loc[i, 'content']
+    print('--stage3')
 
-    s = BM25(answer_list)
+    answer_list_cut = []
+    for ans in tqdm(answer_list):
+        answer_list_cut.append(list(jieba.cut(ans)))
+    s = BM25(answer_list_cut)
+    print('--stage4')
     all_query_ids = []
     all_answers_ids = []
-    for query_id, answers_id in query2answer_id.items():
+    for query_id, answers_id in tqdm(query2answer_id.items()):
         all_query_ids.append(query_id)
         all_answers_ids.append(answers_id)
+    print('--stage5')
     assert len(all_query_ids) == len(all_answers_ids)
     result = []  # 返回的就是candidates答案的索引
-    for query_id in all_query_ids:
+    for query_id in tqdm(all_query_ids):
         result.append(s.simall(list(jieba.cut(id2question[query_id]))))
+    print('--stage6')
 
     labels = []
     for pred, gold in tqdm(zip(result, all_answers_ids)):
@@ -119,6 +129,7 @@ if __name__ == '__main__':
                 label.append(0)
         labels.append(label)
 
+    print('--stage7')
     p1, p3 = PrecisionAtNum(1), PrecisionAtNum(3)
     p5, p10 = PrecisionAtNum(5), PrecisionAtNum(10)
     p20, p30 = PrecisionAtNum(20), PrecisionAtNum(30)
